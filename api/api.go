0f3eac1b1552c431a8b285a1048a864bc7380a16
Marios Antonoudiou <mariosant@sent.com>
2022-02-13 21:34:24 +0000

    Seperate concerns a bit

--- api/api.go
+++ api/api.go
@@ -14,14 +14,22 @@ type Configuration struct {
 	MqttServer *server.Server
 }
 
-func addMqtt(mqttServer *server.Server) gin.HandlerFunc {
+func publishMqtt(mqttServer *server.Server) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Set("mqttServer", mqttServer)
+		go func() {
+			data, _ := c.Value("data").(*gabs.Container)
+
+			organization := data.Path("organization_id").Data().(string)
+			action := data.Path("action").Data().(string)
+
+			mqttServer.Publish(string(organization)+"/"+action, data.Bytes(), false)
+		}()
+
 		c.Next()
 	}
 }
 
-func assignBody(secret string) gin.HandlerFunc {
+func assignBody() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		data, err := gabs.ParseJSONBuffer(c.Request.Body)
 
@@ -34,37 +42,34 @@ func assignBody(secret string) gin.HandlerFunc {
 			return
 		}
 
-		if webhookSecret := data.Path("secret").Data(); webhookSecret != secret {
-			c.String(400, "Could not parse body")
-			c.Abort()
-			
-			return
-		}
-
 		c.Set("data", data)
 
 		c.Next()
 	}
 }
 
-func webhooksHandler(c *gin.Context) {
-	mqttServer, _ := c.Value("mqttServer").(*server.Server)
-
-	go func() {
+func requireSecret(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
 		data, _ := c.Value("data").(*gabs.Container)
 
-		organization := data.Path("organization_id").Data().(string)
-		action := data.Path("action").Data().(string)
+		if webhookSecret := data.Path("secret").Data(); webhookSecret != secret {
+			c.String(400, "Could not parse body")
+			c.Abort()
 
-		mqttServer.Publish(string(organization)+"/"+action, data.Bytes(), false)
-	}()
+			return
+		}
 
+		c.Next()
+	}
+}
+
+func webhooksHandler(c *gin.Context) {
 	c.String(200, "ok")
 }
 
 func CreateServer(config *Configuration) *gin.Engine {
 	server := gin.Default()
 
-	server.POST("/webhooks", assignBody(config.Secret), addMqtt(config.MqttServer), webhooksHandler)
+	server.POST("/webhooks", assignBody(), requireSecret(config.Secret), publishMqtt(config.MqttServer), webhooksHandler)
 	return server
 }

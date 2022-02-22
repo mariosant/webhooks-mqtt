489feb64e7b00c74d07e12af7303fa01d0785f56
Marios Antonoudiou <mariosant@sent.com>
2022-02-22 15:57:54 +0200

    Use ws

--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -1,6 +1,7 @@
 package mqtt
 
 import (
+	"fmt"
 	"log"
 	"strings"
 
@@ -10,12 +11,14 @@ import (
 	"github.com/mochi-co/mqtt/server/listeners"
 )
 
-type Auth struct{
+type Auth struct {
 	secret string
 }
 
 func (a *Auth) Authenticate(user, password []byte) bool {
 	expectedPassword := lib.GeneratePassword(string(user), a.secret)
+
+	fmt.Println("Authentication request: " + string(user))
 
 	return expectedPassword == string(password)
 }
@@ -24,19 +27,21 @@ func (a *Auth) ACL(user []byte, topic string, write bool) bool {
 	isLegitTopic := strings.HasPrefix(topic, string(user))
 	isReadOnly := !write
 
+	fmt.Println("Authorization request: " + string(user))
+
 	return isLegitTopic && isReadOnly
 }
 
 func CreateMqtt(secret string) *m.Server {
 	server := m.New()
 
-	tcp := listeners.NewTCP("t1", ":1883")
+	listener := listeners.NewWebsocket("ws1", ":1883")
 
 	auth := &Auth{
 		secret: secret,
 	}
 
-	err := server.AddListener(tcp, &listeners.Config{
+	err := server.AddListener(listener, &listeners.Config{
 		Auth: auth,
 	})
 
